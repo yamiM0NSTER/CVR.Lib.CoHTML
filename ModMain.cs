c09bf633fd5399d21a7a162bf2039f3bf3977075
yamiM0NSTER <jomex50@gmail.com>
2022-08-23 21:16:49 +0200

    Initial commit

---
+++ ModMain.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVR.Lib.CoHTML {
+  public class ModMain : MelonMod {
+    public static ModMain Instance { get; private set; }
+
+    public MelonLogger.Instance Logger => Instance.LoggerInstance;
+
+    public override void OnApplicationStart() {
+      Instance = this;
+      LoggerInstance.Msg("OnApplicationStart");
+    }
+
+    public override void OnPreSupportModule() {
+      LoggerInstance.Msg("OnPreSupportModule");
+    }
+  }
+}
---
+++ Patches/FileReaderPatches.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using cohtml;
+using System.IO;
+using HarmonyLib;
+
+namespace CVR.Lib.CoHTML.Patches {
+  internal class FileReaderPatches {
+
+    [HarmonyPatch("cohtml.FileReader, Cohtml.Runtime, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null", "NormalizePath")]
+    class NormalizePath {
+      static bool Prefix(ref string path, ref string __result) {
+        if(!path.StartsWith(Constants.cohtmlPrefix, StringComparison.CurrentCulture))
+          return true;
+
+        __result = Path.Combine(Constants.customCoHTMLDirectory, path.Substring(Constants.cohtmlPrefix.Length));
+        ModMain.Instance.Logger.Msg($"__result = {__result}");
+        return false;
+      }
+    }
+  }
+}
